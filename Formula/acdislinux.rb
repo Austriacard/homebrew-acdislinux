71716182e27f871025721af92b5fb5bde72a3d59
AustriaCard <office@austriacard.at>
2023-12-18 08:57:30 +0100

    Upated Formula

--- Formula/acdislinux.rb
+++ Formula/acdislinux.rb
@@ -1,12 +1,12 @@
 class Acdislinux < Formula
   desc "ACDIS PKCS11 Linux"
   homepage "https://www.austriacard.at"
-  url "https://github.com/Austriacard/ACDIS-PKCS11/releases/download/1.3.0/linux-1.3.0.tar.gz"
-  sha256 "7ab579bafd5018702d04c0d59d3168d9f1a3f648a8f57bceac427bb68b8dfa87"
+  url "https://github.com/Austriacard/ACDIS-PKCS11/releases/download/1.4.0/linux-p11module-1.4.0.tar.gz"
+  sha256 "2bba48ce8d04d7be5ba0d51d0e064f428e9bd289ffa04ec4002d4e2f1f0362fa"
 
   bottle do
-    root_url "https://github.com/Austriacard/homebrew-acdislinux/releases/download/acdislinux-1.3.0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6d74c0fb55b1c51594871b8f1f5271bcb0f629dd0272f11c61f5f41082f47c69"
+    root_url "https://github.com/Austriacard/homebrew-acdislinux/releases/download/acdislinux-1.4.0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a2fc78049003fb233b0532a2bcd807e47ca3173090202fdcb4250c1cacc14a62"
   end
   on_linux do
     def install
