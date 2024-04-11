d0384687f5ef49ff6b0814cba3ddab355ae5575e
AustriaCard <office@austriacard.at>
2024-04-11 11:48:41 +0200

    Upated Formula

--- Formula/acdisp11manager.rb
+++ Formula/acdisp11manager.rb
@@ -1,13 +1,8 @@
 class Acdisp11manager < Formula
  desc "ACDIS PKCS11 Manager Linux"
  homepage "https://www.austriacard.at"
- url "https://github.com/Austriacard/ACDIS-PKCS11/releases/download/1.5.0/linux-p11manager-1.5.0.tar.gz"
- sha256 "e913d433d13478eb887396dd638a99583784ca9bb5a0f3e74f358ff26f00bd5a"
-
-  bottle do
-    root_url "https://github.com/Austriacard/homebrew-acdislinux/releases/download/acdisp11manager-1.5.0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6bc03ffe6141f3c752333456c2f89095ed3992ca216aa2f329097a284626cb9f"
-  end
+ url "https://github.com/Austriacard/ACDIS-PKCS11/releases/download/1.6.0/linux-p11manager-1.6.0.tar.gz"
+ sha256 "f837b23fc3d67c71cc0aae3b7badb254ddf36587dc71fffa15e5bfb3d1c0a820"
  depends_on "acdislinux"
  on_linux do
  def install
