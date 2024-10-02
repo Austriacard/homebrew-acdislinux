147dfb6d0ae1b01c987c168d85aca1d8fe0ea633
AustriaCard <office@austriacard.at>
2024-10-02 15:35:05 +0200

    Upated Formula

--- Formula/acdislinux.rb
+++ Formula/acdislinux.rb
@@ -2,7 +2,7 @@ class Acdislinux < Formula
   desc "ACDIS PKCS11 Linux"
   homepage "https://www.austriacard.at"
   url "https://github.com/Austriacard/ACDIS-PKCS11/releases/download/1.7.0/linux-p11module-1.7.0.tar.gz"
-  sha256 "3f0c817b4c0556d7acee9beb37ea95e82b950fa8392b059ca68333d641355b71"
+  sha256 "8ec158f169e582e200a905f7e79345cd70b0b5d8047fff38cd70c054580b5129"
   on_linux do
     def install
       prefix.install Dir["*"]
